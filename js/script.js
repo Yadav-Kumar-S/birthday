c299482638f90909db26b23b38ba3c33d9539a21
Yadav-Kumar-S <25cs301@rmd.ac.in>
2026-06-26 15:19:13 +0530

    upload birthday website

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+    `
+    ❤️ Hey Subha ❤️ <br><br>
+
+    From the moment you came into my life, everything became more beautiful. 🌸<br><br>
+
+    Your smile brightens my darkest days, your voice calms my heart, and your love makes me feel complete. 💕<br><br>
+
+    You are my today, my tomorrow, and my forever. 🌹<br><br>
+
+    🎂 Happy Birthday My Love 🎂<br><br>
+
+    I Love You More Than Words Can Ever Say ❤️
+    `;
+}
+
+function surprise() {
+    document.getElementById("surpriseText").innerHTML =
+    "🎁 You are the best gift life has ever given me. Happy Birthday My Queen ❤️";
+}
